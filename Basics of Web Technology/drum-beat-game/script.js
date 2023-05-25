b810fe1a601adc87b896e8dcfd86f2c5fb6b37e1
Vishnu <vishnuprasadkorada@gmail.com>
2023-05-25 21:14:03 +0530

    drum-beat-game-2

--- Basics of Web Technology/drum-beat-game/script.js
+++ Basics of Web Technology/drum-beat-game/script.js
@@ -3,33 +3,37 @@ const red = document.getElementById("red");
 const blue = document.getElementById("blue");
 const green = document.getElementById("green");
 
-let yellowAudio = new Audio('./cymbals.mp3');
-let redAudio = new Audio('./drum.mp3');
-let blueAudio = new Audio('./drum-2.mp3');
-let greenAudio = new Audio('./drum3.mp3');
+// let yellowAudio = new Audio('./cymbals.mp3');
+// let redAudio = new Audio('./drum.mp3');
+// let blueAudio = new Audio('./drum-2.mp3');
+// let greenAudio = new Audio('./drum3.mp3');
+// yellow.onclick = async() => {
+//     redAudio.pause()
+//     greenAudio.pause()
+//     blueAudio.pause()
+//     yellowAudio.play() 
+// }
+// red.onclick = async() => {
+//     yellowAudio.pause()
+//     greenAudio.pause()
+//     blueAudio.pause()
+//     redAudio.play()
+// }
+// blue.onclick = async() => {
+//     yellowAudio.pause()
+//     greenAudio.pause()
+//     redAudio.pause()
+//     blueAudio.play()
+// }
+// green.onclick = async() => {
+//     yellowAudio.pause()
+//     blueAudio.pause()
+//     redAudio.pause()
+//     greenAudio.play()
+// }
 
-yellow.onclick = async() => {
-    redAudio.pause()
-    greenAudio.pause()
-    blueAudio.pause()
-    yellowAudio.play()
-    
+function generate() {
+    red.classList.toggle('.bright')
 }
-red.onclick = async() => {
-    yellowAudio.pause()
-    greenAudio.pause()
-    blueAudio.pause()
-    redAudio.play()
-}
-blue.onclick = async() => {
-    yellowAudio.pause()
-    greenAudio.pause()
-    redAudio.pause()
-    blueAudio.play()
-}
-green.onclick = async() => {
-    yellowAudio.pause()
-    blueAudio.pause()
-    redAudio.pause()
-    greenAudio.play()
-}
+
+generate()

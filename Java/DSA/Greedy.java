1dbda6a7357aa28ee6ee3ffb0b94a9b27ff0a798
vishnuprasad2004 <vishnuprasadkorada@gmail.com>
2026-05-17 23:31:54 +0530

    greedy problems practice

--- Java/DSA/Greedy.java
+++ Java/DSA/Greedy.java
@@ -1,6 +1,8 @@
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class Greedy {
 
@@ -31,9 +33,79 @@ public class Greedy {
 		return profit;
 	}
 
-	public static void activiySelection() {
+	public static void activiySelection(int[] start, int[] end) {
+		int maxActivities = 0;
+		ArrayList<Integer> ans = new ArrayList<>();
 		
+		maxActivities = 1;
+		ans.add(0);
+		int lastEnded = end[0];
+		
+		for(int i=1; i<end.length; i++) {
+			if(start[i] >= lastEnded) {
+				maxActivities++;
+				ans.add(i);
+				lastEnded = end[i];
+			}
+		}
+
+		System.out.println("Max Activities:" + maxActivities);
+		
+
 	}
+
+	/**
+	 * Given a 2D array intervals[][], where intervals[i] = [starti, endi]. Find the minimum number of intervals need to be removed to make the rest of the intervals non-overlapping.
+	 */
+	public static void getNonOverlappingIntervals(int[][] intervals) {
+		
+		Arrays.sort(intervals, Comparator.comparingDouble((o) -> o[1]));
+		int count = 1;
+		int lastEnded = intervals[0][1];
+		
+		for(int i=1; i<intervals.length; i++) {
+			if(intervals[i][0] >= lastEnded) {
+				count++;
+				lastEnded = intervals[i][1];
+			}
+		}
+
+		System.out.println("Max Intervals: " + count);
+	}
+
+
+	public int[][] insertIntervals(int[][] intervals, int[] newInterval) {
+			List<int[]> list = new ArrayList<>();
+			int n = intervals.length;
+			int i = 0;
+			
+			while(i<n && intervals[i][1] < newInterval[0]) {
+					list.add(intervals[i]);
+					i++;
+			}
+
+			while(i<n && intervals[i][0] < newInterval[1]) {
+					newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+					newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+					i++;
+			}
+			list.add(newInterval);
+
+			while(i<n) {
+					list.add(intervals[i]);
+					i++;
+			}
+
+			int[][] ans = new int[list.size()][2];
+
+			for(int j=0; j<list.size(); j++) {
+					ans[j] = list.get(j);
+			}
+
+			return ans;
+
+	}
+
 
 	public static void main(String[] args) {
 		int values[] = {60, 100, 120};
